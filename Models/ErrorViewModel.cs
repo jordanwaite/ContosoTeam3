802d3b83b3f9274d82a94ef7294f151fa38518c0
Jordan Waite <43104679+jordanwaite@users.noreply.github.com>
2022-09-29 11:41:42 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConfAppTeamThree.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
