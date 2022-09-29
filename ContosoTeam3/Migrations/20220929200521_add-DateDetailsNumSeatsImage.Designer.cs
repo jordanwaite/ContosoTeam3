97ed2c6995166a1230bb402676105d644e40c75d
Jordan Waite <43104679+jordanwaite@users.noreply.github.com>
2022-09-29 16:06:49 -0400

    update session model

--- ContosoTeam3/Migrations/20220929200521_add-DateDetailsNumSeatsImage.Designer.cs
+++ ContosoTeam3/Migrations/20220929200521_add-DateDetailsNumSeatsImage.Designer.cs
@@ -4,6 +4,7 @@ using ContosoTeam3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoTeam3.Migrations
 {
     [DbContext(typeof(ContosoTeam3Context))]
-    partial class ContosoTeam3ContextModelSnapshot : ModelSnapshot
+    [Migration("20220929200521_add-DateDetailsNumSeatsImage")]
+    partial class addDateDetailsNumSeatsImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ContosoTeam3/Migrations/20220929200521_add-DateDetailsNumSeatsImage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoTeam3.Migrations
+{
+    public partial class addDateDetailsNumSeatsImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Session",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Session",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumSeats",
+                table: "Session",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Session");
+
+            migrationBuilder.DropColumn(
+                name: "NumSeats",
+                table: "Session");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Session",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
