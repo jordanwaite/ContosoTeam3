7905111ef240885dc7a4c29cb2a3f4623ceab7c5
Jordan Waite <43104679+jordanwaite@users.noreply.github.com>
2022-09-29 16:08:26 -0400

    update speaker db model

---
+++ ContosoTeam3/Migrations/20220929200803_add-FNameLname.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoTeam3.Migrations
+{
+    public partial class addFNameLname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Speaker");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Speaker",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Speaker",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Speaker");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Speaker");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Speaker",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
